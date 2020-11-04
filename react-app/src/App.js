c37b876dec3f47f8d72101e1b15e89fafcfc158a
Jan Savli <jan.savli@gmail.com>
2020-11-04 09:59:20 +0100

    Add web components to React

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+const value = { label: 'Canadax' };
+const options = [
+  {label: 'Canadax'},
+  {label: 'Bum'},
+  {label: 'Luxembourg'}
+];
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <zen-dropdown-simple val={value} options={options} input2="input" />
+        <zen-simple name="Marry" />
         <p>
+          Jan
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { defineCustomElements } from '@chavlji/stencil/loader';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// Inject into the window object
+defineCustomElements(window);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
